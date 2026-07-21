6b54bb6c2b09a475f284a11791bae99c8901f7f8
Samarth Kasalkar <lets.talk.samarth@gmail.com>
2026-07-21 23:31:25 +0530

    Fixing registration and login

--- client/src/lib/api.js
+++ client/src/lib/api.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://artverse-backend-cg83.onrender.com//api",
+  baseURL: "https://artverse-backend-cg83.onrender.com/api",
 });
 
 export const IMAGE_URL =
-  "https://artverse-backend-cg83.onrender.com//uploads/";
+  "https://artverse-backend-cg83.onrender.com/uploads/";
 
 export default API;
